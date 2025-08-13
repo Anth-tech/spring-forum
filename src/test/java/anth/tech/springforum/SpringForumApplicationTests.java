cdcc2dd534d0a45aa9e674736f6579d5e0de18b8
Anthony Chapman <anthonychapman535@gmail.com>
2025-08-12 23:08:33 -0500

    init

---
+++ src/test/java/anth/tech/springforum/SpringForumApplicationTests.java
@@ -0,0 +1,13 @@
+package anth.tech.springforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringForumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
